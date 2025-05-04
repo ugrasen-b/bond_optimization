4d0e9528387c35de72a85846c3a362b029890831
ugrasen-b <ugrasen.banchhor@gmail.com>
2025-05-04 07:35:49 +0530

    Adding main.py file, only as a placeholder for now

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  3 21:05:00 2025
+
+@author: ugras
+"""
+
+from bond_optimization.data_loader import load_yield_curve
+
+df = load_yield_curve("yc2024.csv","input")
