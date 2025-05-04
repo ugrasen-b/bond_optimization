b978822557a868df0471072e475d1381f0f30258
ugrasen-b <ugrasen.banchhor@gmail.com>
2025-05-04 09:26:53 +0530

    Adding a config file for configuration static values

---
+++ bond_optimization/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  4 08:02:34 2025
+
+@author: ugras
+"""
+
+EXPECTED_COLUMNS = [
+    '1M', '3M', '6M', '1Y', '2Y', '3Y', '5Y', '7Y', '10Y', '20Y', '30Y'
+]
